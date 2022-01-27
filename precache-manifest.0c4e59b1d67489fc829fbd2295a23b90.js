9e779f8a48963c62530c85ee5106120c0ff3f68d
Rohit <rdabas@cdot.in>
2022-01-27 09:48:06 -0700

    Updates

---
+++ precache-manifest.0c4e59b1d67489fc829fbd2295a23b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ba959638c78ff953d8",
+    "url": "/portfolio1/static/css/main.081defbc.chunk.css"
+  },
+  {
+    "revision": "b5ba959638c78ff953d8",
+    "url": "/portfolio1/static/js/main.b5ba9596.chunk.js"
+  },
+  {
+    "revision": "ad34a9230be13e422ac5",
+    "url": "/portfolio1/static/js/runtime~main.ad34a923.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio1/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c362eeb71fc6a935a30637000ba7e4ef",
+    "url": "/portfolio1/index.html"
+  }
+];
